43dfbfc11855234b30252490dbce2c95d59aa582
Roman Dvornov <rdvornov@gmail.com>
2015-11-23 18:43:32 +0300

    fix broken tests and use `--target none` for build test

--- test/build/css-optimize-names.js
+++ test/build/css-optimize-names.js
@@ -3,38 +3,38 @@ var path = require('path');
 var assert = require('assert');
 
 describe('build with --css-optimize-names', function(){
-  process.env.PWD = __dirname + '/env/app';
+  it.only('should replace classes but don\'t ids', function(){
+    var cwd = __dirname + '/env/app';
+    process.env.PWD = cwd;
 
-  it('should replace classes but don\'t ids', function(){
-    return cli.build.run(['--silent', '--css-optimize-names'])
+    return cli.build.run(['--silent', '--target', 'none', '--css-optimize-names'])
       .then(function(flow){
-        var outputMap = flow.files.queue.reduce(function(map, file){
-          if (file.outputFilename)
-            map[file.outputFilename] = file;
+        var outputMap = flow.result.reduce(function(map, file){
+          map[path.relative(cwd, file.path).replace(/\\/g, '/')] = file;
           return map;
         }, {});
 
         function content(filename){
           if (outputMap[filename])
-            return outputMap[filename].outputContent || '';
+            return outputMap[filename].content || '';
         }
 
         // index.html
-        assert(!/html-class-test/.test(content('index.html')));
-        assert(/class="b"/.test(content('index.html')));
-        assert(/id="html-id-test"/.test(content('index.html')));
+        assert(!/html-class-test/.test(content('build/index.html')));
+        assert(/class="b"/.test(content('build/index.html')));
+        assert(/id="html-id-test"/.test(content('build/index.html')));
 
         // style.css
-        assert(!/\.file-class-test/.test(content('style.css')));
-        assert(/\.a \{/.test(content('style.css')));
-        assert(/\#file-id-test/.test(content('style.css')));
-        assert(!/\.inline-class-test/.test(content('style.css')));
-        assert(/\.c \{/.test(content('style.css')));
-        assert(/\#inline-id-test/.test(content('style.css')));
+        assert(!/\.file-class-test/.test(content('build/style.css')));
+        assert(/\.a \{/.test(content('build/style.css')));
+        assert(/\#file-id-test/.test(content('build/style.css')));
+        assert(!/\.inline-class-test/.test(content('build/style.css')));
+        assert(/\.c \{/.test(content('build/style.css')));
+        assert(/\#inline-id-test/.test(content('build/style.css')));
 
         // script.js
-        assert(!/"tmpl-class-test"/.test(content('script.js')));
-        assert(/"tmpl-id-test"/.test(content('script.js')));
+        assert(!/"tmpl-class-test"/.test(content('build/script.js')));
+        assert(/"tmpl-id-test"/.test(content('build/script.js')));
       });
   });
 });
